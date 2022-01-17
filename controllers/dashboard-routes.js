6f37293122815a085017da27c625e46e2ed53699
Chris Phillips <cp.phillips15@gmail.com>
2022-01-16 21:53:02 -0500

    added dashboard route to call for posts comments and users, then render the dashboard.handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,8 +2,46 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+// /dahsboard route maps all posts to be seen
 router.get('/', withAuth, (req, res) => {
-    console.log("dashboard-routes is here");
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        atttributes: [
+            'id',
+            'title',
+            'content',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain:true }));
+        res.render('dashboard', {posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
 
 
