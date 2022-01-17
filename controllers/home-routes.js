fac4befa63b750dd3d54a0ca185cbdbdca30ee28
Chris Phillips <cp.phillips15@gmail.com>
2022-01-16 20:56:51 -0500

    added /post/:id to home routes to run tests on /post with single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
       attributes: [
         'id',
         'title',
-        'description',
+        'content',
         'created_at'
       ],
       include: [
@@ -49,7 +49,49 @@ router.get('/', (req, res) => {
     });
 })
 
-
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    // If there is no post
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found'});
+      return;
+    }
+    const post = dbPostData.get({ plain: true });
+    res.render('single-post', { 
+      post,
+      loggedIn: req.session.loggedIn
+      });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 
 router.get('/login', (req, res) => {
