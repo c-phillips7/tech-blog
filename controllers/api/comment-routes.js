642f87fb9f588f4efb6e412401d4bfff1cfb5efd
Chris Phillips <cp.phillips15@gmail.com>
2022-01-16 19:40:48 -0500

    added planned route files along with the necessary inport/exposts. Also added commented out planned requests

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//get reuqest to fid all
+
+// post request to make a new post
+
+// delete request to destroy comment
+
+module.exports = router;
---
+++ controllers/api/post-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// get request for findAll users (NOTE: must include all necessary parts)
+
+// get post by id
+
+//post requestto create new post in db
+
+// Update post by ID
+
+module.exports = router;
