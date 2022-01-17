2a593f6b93e436f9da4ab732b83e92af71687a3c
Chris Phillips <cp.phillips15@gmail.com>
2022-01-16 21:23:21 -0500

    added post request to make new comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -18,6 +18,20 @@ router.get('/', (req, res) => {
 });
 
 // post request to make a new post
+router.post('/', (req, res) => {
+    if(req.session) {
+        Comment.create({
+          comment_text: req.body.comment_text,
+          user_id: req.session.user_id,
+          post_id: req.body.post_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+        }
+});
 
 // delete request to destroy comment
 
